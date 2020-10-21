962890894134e0fea5f545cc9080c511cd7bc910
Cody Westerman <61666737+CodyAnts@users.noreply.github.com>
2020-10-21 14:37:17 +1300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function button1(){
+    document.getElementById("myImage").src="images/match-lit.png";
+}
+
+function button2(){
+    document.getElementById("myImage").src="images/match-unlit.png";
+}
